94114e6da39ec09a3b9ee8b75f08a74720340d0c
Muddyblack <70057554+Muddyblack@users.noreply.github.com>
2026-09-11 11:17:53 +0200

    fix: update flake.lock and flake.nix for improved package management and compatibility

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
       metadata = builtins.fromJSON (builtins.readFile ./package/metadata.json);
     in {
       packages = forAllSystems (system:
-        let pkgs = import nixpkgs { inherit system; };
+        let pkgs = nixpkgs.legacyPackages.${system};
         in {
           default = pkgs.stdenvNoCC.mkDerivation {
             pname = "tagesschau-widget";
@@ -44,12 +44,13 @@
         });
 
       apps = forAllSystems (system:
-        let pkgs = import nixpkgs { inherit system; };
+        let pkgs = nixpkgs.legacyPackages.${system};
         in {
           view = {
             type = "app";
             program = toString (pkgs.writeShellScript "view" ''
-              exec nix shell nixpkgs#kdePackages.plasma-sdk -c plasmoidviewer \
+              export PATH=${pkgs.lib.makeBinPath [ pkgs.kdePackages.plasma-sdk ]}:"$PATH"
+              exec plasmoidviewer \
                 -a "$PWD/package" -f "''${1:-planar}"
             '');
           };
@@ -69,7 +70,7 @@
         });
 
       devShells = forAllSystems (system:
-        let pkgs = import nixpkgs { inherit system; };
+        let pkgs = nixpkgs.legacyPackages.${system};
         in {
           default = pkgs.mkShell {
             name = "tagesschau-widget-dev";
